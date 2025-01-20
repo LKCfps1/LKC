65d7e2ef1668174ee4c96919e6193ec1e713b9bc
LKCfps1 <dondada69699@outlook.com>
2025-01-20 14:02:54 -0800

    Add files via upload

---
+++ lkc_selldrugs/bridge/qb/server.lua
@@ -0,0 +1,63 @@
+if GetResourceState('qb-core') ~= 'started' then return end
+
+QBCore = exports['qb-core']:GetCoreObject()
+
+function GetPlayerFromId(id)
+    return QBCore.Functions.GetPlayer(id)
+end
+
+function AddAccountMoney(source, account, amount)
+    local player = GetPlayerFromId(source)
+    if account == 'black_money' then
+        -- Make the item consistent without creating a new "info" value for each sale
+        return player.Functions.AddItem('black_money', amount, false)
+    else
+        return player.Functions.AddMoney(account, amount, 'Narcotics')
+    end
+end
+
+
+function AddCash(source, amount)
+    local player = GetPlayerFromId(source)
+    return player.Functions.AddMoney('cash', amount, 'Narcotics')
+end
+
+function RemoveItem(source, name, amount)
+    local player = GetPlayerFromId(source)
+    TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items[name], 'remove', amount)
+    return player.Functions.RemoveItem(name, amount)
+end
+
+function GetItemCount(source, name)
+    local player = GetPlayerFromId(source)
+    local item = player.Functions.GetItemByName(name)
+    if item ~= nil then
+        return item.amount
+    else
+        return 0
+    end
+end
+
+function GetItemLabel(source, name)
+    local player = GetPlayerFromId(source)
+    local item = player.Functions.GetItemByName(name)
+    if item ~= nil then
+        return item.label
+    else
+        return 0
+    end
+end
+
+lib.callback.register('fsg_selldrugs:getPoliceCount', function(source)
+    count = 0
+    if Config.requiredCops then
+        for _, v in pairs(QBCore.Functions.GetQBPlayers()) do
+            if v.PlayerData.job.name == "police" and v.PlayerData.job.onduty then
+                count = count + 1
+            end
+        end
+        return count
+    else
+        return count
+    end
+end)
---
+++ lkc_selldrugs/server/server.lua
@@ -0,0 +1,58 @@
+RegisterCommand('dealer', function(source, args, rawcommand)
+    drugToSell = {
+        type = '',
+        label = '',
+        count = 0,
+        i = 0,
+        price = 0,
+    }
+    for k, v in pairs(Config.drugs) do
+        local item = GetItemLabel(source, k)
+            
+        if item == nil then
+            return        
+        end
+            
+        count = GetItemCount(source, k)
+        drugToSell.i = drugToSell.i + 1
+        drugToSell.type = k
+        drugToSell.label = item
+        
+        if count >= 5 then
+            drugToSell.count = math.random(5, 10)
+        elseif count > 0 then
+            drugToSell.count = math.random(1, count)
+        end
+
+        if drugToSell.count ~= 0 then
+            drugToSell.price = drugToSell.count * v + math.random(1, 300)
+            TriggerClientEvent('stasiek_selldrugsv2:findClient', source, drugToSell)
+            break
+        end
+        
+        if TableSizeOf(Config.drugs) == drugToSell.i and drugToSell.count == 0 then
+            TriggerClientEvent('ox_lib:notify', source, {title = 'Drugs', description = Config.notify.nodrugs, duration = 8000, position = 'center-right', icon = 'pills'})
+        end
+    end
+end, false)
+
+RegisterServerEvent('stasiek_selldrugsv2:pay')
+AddEventHandler('stasiek_selldrugsv2:pay', function(drugToSell)
+    local src = source
+    local count = GetItemCount(src, drugToSell.type)
+    if count >= drugToSell.count then
+        RemoveItem(src, drugToSell.type, drugToSell.count)
+        if Config.account == 'money' then
+            AddCash(src, drugToSell.price)
+        else
+            AddAccountMoney(src, Config.account, drugToSell.price)
+        end
+    else
+        TriggerClientEvent('ox_lib:notify', src, {title = 'Drugs', description = Config.notify.nodrugs, duration = 8000, position = 'center-right', icon = 'pills'})
+    end
+end)
+
+RegisterServerEvent('stasiek_selldrugsv2:notifycops')
+AddEventHandler('stasiek_selldrugsv2:notifycops', function(drugToSell)
+    TriggerClientEvent('stasiek_selldrugsv2:notifyPolice', -1, drugToSell.coords)
+end)
